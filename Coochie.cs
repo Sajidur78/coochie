e6e6fe282bb286efa21c3589d0e0f7ef72f72b1f
Sajid <sajidur78@gmail.com>
2019-01-07 19:55:07 +0600

    THIS DOESNT WANT TO FUCKING WORK

---
+++ Coochie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Coochie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 3; i > 0; i--)
+            {
+                Console.WriteLine($"Crashing in {i}...");
+                Thread.Sleep(1000);
+            }
+            throw new NotImplementedException("Crashing not yet implemented!");
+        }
+    }
+}
